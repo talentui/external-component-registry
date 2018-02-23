ffe0199803da36b8581d8ab12d869d681f456e89
lihe3 <lihe3@corp.beisencorp.com>
2018-02-23 17:50:47 +0800

    build:add hook

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const obj = {};
+let hook = null;
 const componentRegistry = {
     set: function(key,value) {
         obj[key] = value;
+        hook && hook(obj);
     },
     get: function() {
         return obj;
+    },
+    setHook: function(func){
+        hook = func;
     }
 };
 export default componentRegistry;
